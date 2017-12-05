9f22a4d913f61c9418e5de2a842552620c59dda2
RudyErnesto <32149729+RudyErnesto@users.noreply.github.com>
2017-12-05 17:04:17 -0400

    proyecto del diplomado de ios

--- DiplomadoIOs/ViewController.swift
+++ DiplomadoIOs/ViewController.swift
@@ -7,11 +7,47 @@
 //
 
 import UIKit
-
+struct Country: Decodable {
+    let name: String
+    let capital: String
+    let region: String
+}
 class ViewController: UIViewController {
-
+    @IBOutlet weak var pais: UILabel!
+    @IBOutlet weak var namecapital: UILabel!
+    @IBOutlet weak var nameregion: UILabel!
+    
+    
+    var countries = [Country]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("paso")
+        let jsonurl = "https://restcountries.eu/rest/v2/all"
+        
+        let url = URL(string: jsonurl)
+        
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            do{
+                
+                self.countries = try JSONDecoder().decode([Country].self, from: data!)
+                
+                let randomIndex = Int(arc4random_uniform(UInt32(self.countries.count)))
+                
+                let random = self.countries[randomIndex]
+                
+                self.pais.text = random.name
+                self.namecapital.text = random.region
+                self.nameregion.text = random.capital
+            }
+                
+            catch{
+                
+                print("error")
+                
+            }
+            
+            }.resume()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +55,5 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
